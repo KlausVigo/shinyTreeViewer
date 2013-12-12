efe1ff341b63ade72628660e81149d88a20e61dc
KlausVigo <klaus.schliep@gmail.com>
2013-12-12 13:08:45 +0100

    First version of shinyTreeeViewer

---
+++ server.R
@@ -0,0 +1,123 @@
+library(shiny)
+library(ape)
+library(phangorn)
+
+
+
+readTrees <- function(file, format="phylip"){
+    if(format=="phylip")tree = read.tree(file)
+    else tree = read.nexus(file)
+    if(class(tree)=="phylo")tree = list(tree)
+    tree
+}
+
+shinyServer(function(input, session, output) {
+
+    treeInput <- reactive({
+        readTrees(input$file1$datapath, input$format)
+    })
+
+
+    output$treeControls <- renderUI({
+        inFile <- input$file1        
+        if (is.null(inFile))
+            return(NULL)        
+        tree <- treeInput()
+        ntrees <- length(tree) 
+        if(ntrees>1)
+        sliderInput("tpos", "Tree:", min=1, max=ntrees, value=1, step=1)    
+    })
+    
+    pos <- reactive({
+        input$tpos
+    })
+
+    output$phyloPlot <- renderPlot({
+
+        inFile <- input$file1        
+        if (is.null(inFile))
+            return(NULL)
+        
+        trees <- treeInput()
+        
+        if(length(trees)==1)tree = trees[[1]]
+        else tree = trees[[pos()]]
+        
+        if(input$midpoint) tree <- midpoint(tree)
+        plot.phylo(tree, 
+                type = input$type,
+                show.tip.label = input$showTips,
+                show.node.label = input$showNodes,
+                direction=input$direction, 
+                rotate.tree = input$rotate,
+                use.edge.length = input$edgeLength,   
+                edge.width = input$edgewidth, 
+                edge.lty =input$lty,   
+                edge.color =input$edgecolor,
+                tip.color = input$tipcolor)
+        if(input$scalebar) add.scale.bar()
+    })
+    output$downloadPlot <- downloadHandler(
+        filename = function() { paste(input$filename, '.png', sep='') },
+        content = function(file) {
+            png(file)
+            trees <- treeInput()
+            
+            if(length(trees)==1)tree = trees[[1]]
+            else tree = trees[[pos()]]
+            if(input$midpoint) tree <- midpoint(tree)
+            plot.phylo(tree, 
+                       type = input$type,
+                       show.tip.label = input$showTips,
+                       show.node.label = input$showNodes,
+                       direction=input$direction, 
+                       rotate.tree = input$rotate,
+                       use.edge.length = input$edgeLength,   
+                       edge.width = input$edgewidth, 
+                       edge.lty =input$lty,   
+                       edge.color =input$edgecolor,
+                       tip.color = input$tipcolor)
+            if(input$scalebar) add.scale.bar()
+            dev.off()
+        })
+    
+    output$rcode <- renderPrint({       
+        inFile <- input$file1        
+        if (is.null(inFile))
+            return(NULL)
+        trees <- treeInput()
+        cat("library(ape) \n", sep="")
+        cat("library(phangorn) \n", sep="")
+        if(length(trees)==1){
+            if(input$format=='phylip') cat("tree = read.tree('",input$file1$name,"') \n", sep="")
+            else cat("tree = read.nexus(",input$file1,") \n", sep="")
+        }
+        else {
+            if(input$format=='phylip') cat("trees = read.tree('",input$file1$name,"') \n", sep="")
+            else cat("trees = read.nexus(",input$file1,") \n", sep="")
+            cat("tree = trees[[",pos(),"]] \n", sep="")
+        }
+        if(input$midpoint) cat("tree = midpoint(tree)\n", sep="")
+        if(input$showTips==FALSE)showTips = ", show.tip.label=FALSE"
+        else showTips = ""
+        if(input$showNodes==TRUE)showNodes = ", show.node.label=TRUE"
+        else showNodes = ""
+        if(input$direction!="rightwards") direction = paste(", direction=", input$direction, sep="")
+        else direction=""
+        if(input$rotate!=0) rotate = paste(", rotate.tree=", input$rotate, sep="")
+        else rotate=""    
+        if(input$edgeLength==FALSE) edgeLength = paste(", use.edge.length=FALSE")
+        else edgeLength=""
+        if(input$edgewidth!=1) edgewidth = paste(", edge.width=", input$edgewidth, sep="")
+        else edgewidth=""
+        if(input$lty!=1) lty = paste(", edge.lty=", input$lty, sep="")
+        else lty=""
+        if(input$edgecolor!="black") edgecolor = paste(", edge.color='", input$edgecolor, "'", sep="")
+        else edgecolor=""
+        if(input$tipcolor!="black") tipcolor = paste(", tip.color='", input$tipcolor, "'", sep="")
+        else tipcolor=""        
+        cat("plot.phylo(tree, type='",input$type,"'", showTips, showNodes, direction, rotate, edgeLength, edgewidth, lty, edgecolor, tipcolor, ") \n", sep = "")
+        if(input$scalebar) cat("add.scale.bar() \n", sep="")
+    })
+    
+})
---
+++ ui.R
@@ -0,0 +1,52 @@
+shinyUI(pageWithSidebar(
+    headerPanel("shinyTreeViewer"),
+    sidebarPanel(
+        
+        fileInput('file1', 'Choose tree file'),
+        radioButtons('format', 'File format',
+                     c(phylip='phylip',
+                       nexus='nexus'),
+                     'phylip'), 
+        tags$hr(),        
+        textInput('filename', "Filename"),
+        downloadButton('downloadPlot', 'Export Plot'),    
+        tags$hr(),
+# Layout        
+# maybe submitButton("Update plot!") for large trees
+        uiOutput("treeControls"),
+        
+        selectInput("type", "Choose a type:", 
+                    choices = c('phylogram', 'cladogram', 'fan', 'unrooted', 'radial')), 
+        conditionalPanel(
+            condition = "input.type == 'phylogram' || input.type == 'cladogram'",
+            selectInput('direction', 'Direction',
+                        c('rightwards', 'leftwards', 'upwards', 'downwards'))
+        ),
+        conditionalPanel(
+            condition = "input.type == 'fan' || input.type == 'unrooted' || input.type == 'radial'",
+            numericInput("rotate", "Rotate:", value=0)
+        ),         
+        checkboxInput("scalebar", "scale bar", TRUE),  
+        checkboxInput("midpoint", "Midpoint rooting", FALSE),
+        tags$hr(),
+#Tips        
+        checkboxInput("showTips", "Show tip labels", TRUE),
+        textInput("tipcolor", "Tip color:", value="black"),
+#Nodes
+        checkboxInput("showNodes", "Show node labels (bs)", FALSE),
+        tags$hr(),
+#Edges  
+        checkboxInput("edgeLength", "Use edge length", TRUE),
+        numericInput("edgewidth", "Edge width", value=1),
+        textInput("edgecolor", "Edge color:", value="black"),
+        numericInput("lty", "Line type:", value=1)
+        
+    ),
+    mainPanel(
+        tabsetPanel(
+            tabPanel("Plot", plotOutput("phyloPlot")), 
+            tabPanel("R code", verbatimTextOutput("rcode")) 
+        )
+
+    )
+))
