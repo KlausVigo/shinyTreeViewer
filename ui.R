18abff5bfaa82f6c972fbcd30a4256f17d810a13
klash <klaus.schliep@gmail.com>
2014-10-25 15:58:34 -0400

    relative paths to pics

--- ui.R
+++ ui.R
@@ -30,11 +30,11 @@ shinyUI(navbarPage("shinyTreeViewer",
         sidebarLayout(
             sidebarPanel(     
                 HTML("<div id=\"ptype\" class=\"btn-group\">
-<button id=\"phylogram\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"phylogram.png\" width=\"40\" height=\"40\" alt=\"phylogram\"/>\n</button>\n
-<button id=\"unrooted\" type=\"button\" class=\"btn action-button\">\n  <img src=\"unrooted.png\" width=\"40\" height=\"40\" alt=\"unrooted\"/>\n</button>
-<button id=\"fan\" type=\"button\" class=\"btn action-button\">\n  <img src=\"fan.png\" width=\"40\" height=\"40\" alt=\"fan\"/>\n</button>
-<button id=\"cladogram\" type=\"button\" class=\"btn action-button\">\n  <img src=\"cladogram.png\" width=\"40\" height=\"40\" alt=\"cladogram\"/>\n</button>
-<button id=\"radial\" type=\"button\" class=\"btn action-button\">\n  <img src=\"radial.png\" width=\"40\" height=\"40\" alt=\"radial\"/>\n</button>
+<button id=\"phylogram\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"./phylogram.png\" width=\"40\" height=\"40\" alt=\"phylogram\"/>\n</button>\n
+<button id=\"unrooted\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./unrooted.png\" width=\"40\" height=\"40\" alt=\"unrooted\"/>\n</button>
+<button id=\"fan\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./fan.png\" width=\"40\" height=\"40\" alt=\"fan\"/>\n</button>
+<button id=\"cladogram\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./cladogram.png\" width=\"40\" height=\"40\" alt=\"cladogram\"/>\n</button>
+<button id=\"radial\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./radial.png\" width=\"40\" height=\"40\" alt=\"radial\"/>\n</button>
 </div>"),   
                 # Layout        
                 # maybe submitButton("Update plot!") for large trees
@@ -55,10 +55,10 @@ shinyUI(navbarPage("shinyTreeViewer",
                 tags$hr(),
                 #Labels
                 HTML("<div id=\"font\" class=\"btn-group\">
-<button id=\"font1\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"font1.svg\" width=\"40\" height=\"40\" alt=\"phylogram\"/>\n</button>\n
-<button id=\"font2\" type=\"button\" class=\"btn action-button\">\n  <img src=\"font2.svg\" width=\"40\" height=\"40\" alt=\"unrooted\"/>\n</button>
-<button id=\"font3\" type=\"button\" class=\"btn action-button\">\n  <img src=\"font3.svg\" width=\"40\" height=\"40\" alt=\"fan\"/>\n</button>
-<button id=\"font4\"type=\"button\" class=\"btn action-button\">\n  <img src=\"font4.svg\" width=\"40\" height=\"40\" alt=\"cladogram\"/>\n</button>
+<button id=\"font1\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"./font1.svg\" width=\"40\" height=\"40\" alt=\"phylogram\"/>\n</button>\n
+<button id=\"font2\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./font2.svg\" width=\"40\" height=\"40\" alt=\"unrooted\"/>\n</button>
+<button id=\"font3\" type=\"button\" class=\"btn action-button\">\n  <img src=\"./font3.svg\" width=\"40\" height=\"40\" alt=\"fan\"/>\n</button>
+<button id=\"font4\"type=\"button\" class=\"btn action-button\">\n  <img src=\"./font4.svg\" width=\"40\" height=\"40\" alt=\"cladogram\"/>\n</button>
 </div>"),    
                 #Tips        
                 checkboxInput("showTips", "Tip labels", TRUE),
