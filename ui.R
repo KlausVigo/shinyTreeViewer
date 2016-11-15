42fca0fd79a0ef4ec3d159fa359f83ee2cd26c50
klash <klaus.schliep@gmail.com>
2016-11-15 18:59:00 -0500

    improved interface

--- ui.R
+++ ui.R
@@ -11,25 +11,40 @@ shinyUI(navbarPage("shinyTreeViewer",
                     radioButtons("datasource", HTML('<font size="4"> Choose data source:</font>'),
                                     list("Example: annotated tree"="exTree", 
                                          "Example: MCMC sample"="exMCMC",
-                                         "Input file"="file", 
+                                         "Import tree"="file", 
                                          "Simulate trees"="simulate")),   
+                    
+                    conditionalPanel(condition = "input.datasource=='file'",
+                                     fileInput("file1", "Load tree file"),
+                                     actionButton("downloadButton", "update plot") #icon("refresh")),
+                    ),
+                    
+                    conditionalPanel(condition = "input.datasource=='simulate'",
+                                     #                    h3("Simulate tree"),    
+                                     numericInput("ntips", "Number of tips", value=10, min=3),   
+                                     numericInput("ntrees", "Number of trees", value=1, min=1),
+                                     checkboxInput("isrooted", "Rooted", TRUE),
+                                     actionButton("simulateButton", "update plot") #  icon("refresh"))
+                    ),
+
+                    
                        
-                       
-                    h3("Import tree"),   
-                    fileInput('file1', 'Import tree file'),
-                    actionButton("downloadButton", "update plot"), #icon("refresh")),
+#                    h3("Import tree"),   
+#                    fileInput('file1', 'Import tree file'),
+#                    actionButton("downloadButton", "update plot"), #icon("refresh")),
+                    
                     tags$hr(),
                     h3("Export tree"), 
                     textInput('filename', 'Filename'),
                     selectInput('ExportFormat', '', choices = c('pdf', 'ps', 'tex', 'svg', 'png', 'jpg', 'bmp' )),
                     downloadButton('downloadPlot', 'Export Plot')       
                 ),
-                column(6,
-                    h3("Simulate tree"),    
-                    numericInput("ntips", "Number of tips", value=10, min=3),   
-                    numericInput("ntrees", "Number of trees", value=1, min=1),
-                    checkboxInput("isrooted", "Rooted", TRUE),
-                    actionButton("simulateButton", "update plot") #  icon("refresh"))
+                column(6 #,
+#                    h3("Simulate tree"),    
+#                    numericInput("ntips", "Number of tips", value=10, min=3),   
+#                    numericInput("ntrees", "Number of trees", value=1, min=1),
+#                    checkboxInput("isrooted", "Rooted", TRUE),
+#                    actionButton("simulateButton", "update plot") #  icon("refresh"))
                 )        
             )
         )     
